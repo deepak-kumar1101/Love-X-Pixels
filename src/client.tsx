869941dc3644ce5f72d1e16838c6c744d3daf1d8
deepak-kumar1101 <deepsrajput1101@gmail.com>
2026-08-11 18:11:55 +0530

    Update

--- src/client.tsx
+++ src/client.tsx
@@ -1,7 +1,14 @@
-import { hydrateRoot } from "react-dom/client";
+import { hydrateRoot, createRoot } from "react-dom/client";
 import { StartClient } from "@tanstack/react-start/client";
 import { createRouter } from "./router";
 
 const router = createRouter();
+const rootElement = document.getElementById("root");
 
-hydrateRoot(document, <StartClient router={router} />);
+if (rootElement) {
+  if (rootElement.hasChildNodes()) {
+    hydrateRoot(rootElement, <StartClient router={router} />);
+  } else {
+    createRoot(rootElement).render(<StartClient router={router} />);
+  }
+}
