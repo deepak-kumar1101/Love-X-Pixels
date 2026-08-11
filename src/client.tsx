118098c40545d43563d87f4ec82fd112f332a9cf
deepak-kumar1101 <deepsrajput1101@gmail.com>
2026-08-11 18:27:50 +0530

    Updated

---
+++ src/client.tsx
@@ -0,0 +1,7 @@
+import { hydrateRoot } from "react-dom/client";
+import { RouterProvider } from "@tanstack/react-router";
+import { getRouter } from "./router";
+
+const router = getRouter();
+
+hydrateRoot(document, <RouterProvider router={router} />);
